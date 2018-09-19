d0df0f199a91375931164b6749ba289faa0c73bb
robin <robintsunny@gmail.com>
2018-09-18 17:29:50 -0700

    User Routes and Validations

--- config/passport.js
+++ config/passport.js
@@ -9,8 +9,16 @@ options.jwtFromRequest = ExtractJwt.fromAuthHeaderAsBearerToken();
 options.secretOrKey = keys.secretOrKey;
 
 module.exports = passport => {
-    passport.use(new JwtStrategy(options, (payload, done) => {
-        // This payload includes the items we specified earlier
-        console.log(payload);
-    }));
+  passport.use(new JwtStrategy(options, (payload, done) => {
+    User.findById(jwt_payload.id)
+      .then(user => {
+        if (user) {
+          // return the user to the frontend
+          return done(null, user);
+        }
+        // return false since there is no user
+        return done(null, false);
+      })
+      .catch(err => console.log(err));
+  }));
 };
