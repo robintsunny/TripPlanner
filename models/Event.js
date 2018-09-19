f7fcbb59253772a2755f1e80ec32016e22918a2b
robin <robintsunny@gmail.com>
2018-09-18 17:33:57 -0700

    Events Routes and Validations

---
+++ models/Event.js
@@ -0,0 +1,22 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const EventSchema = new Schema({
+  user: {
+    type: Schema.Types.ObjectId,
+    ref: 'users'
+  },
+  text: {
+    type: String,
+    required: true
+  },
+  name: {
+    type: String
+  },
+  date: {
+    type: Date,
+    default: Date.now
+  }
+});
+
+module.exports = Event = mongoose.model('event', EventSchema);
--- routes/api/events.js
+++ routes/api/events.js
@@ -1,8 +1,39 @@
-const express = require("express");
+const express = require('express');
 const router = express.Router();
+const mongoose = require('mongoose');
+const passport = require('passport');
 
-router.get("/events", (req, res) => res.json({
-    msg: "This is the events route"
-}));
+const Event = require('../../models/Event');
+const validateEventInput = require('../../validation/event');
 
-module.exports = router;
+router.get('/', (req, res) => {
+  Event.find()
+    .sort({ date: -1 })
+    .then(events => res.json(events))
+    .catch(err => res.status(404).json({ noeventsfound: 'No events found' }));
+});
+
+router.get('/:id', (req, res) => {
+  Event.findById(req.params.id)
+    .then(event => res.json(event))
+    .catch(err =>
+      res.status(404).json({ noeventfound: 'No event found with that ID' })
+    );
+});
+
+router.post('/', passport.authenticate('jwt', { session: false }),(req, res) => {
+    const { errors, isValid } = validateEventInput(req.body);
+
+    if (!isValid) {
+      return res.status(400).json(errors);
+    }
+
+    const newEvent = new Event({
+      text: req.body.text,
+      name: req.body.name,
+      user: req.user.id
+    });
+
+    newEvent.save().then(event => res.json(event));
+  }
+);
